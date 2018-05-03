b0b8dd699fc477e5a55a7deb826810c8adfed24a
DESKTOP-UCCMCGD\takab <takabsk55@yahoo.co.jp>
2018-05-03 17:10:26 +0900

    作り直し

---
+++ Assets/Script/Cursorlock.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursorlock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// マウスカーソルを画面内にロックする。
+		Screen.lockCursor = true;
+//		Cursor.lockState = CursorLockMode.Locked;
+		// マウスカーソルを消去する
+		Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
