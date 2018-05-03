9114a852ef47962d6c5030567aef85338e49fc0c
DESKTOP-UCCMCGD\takab <takabsk55@yahoo.co.jp>
2018-05-03 18:18:05 +0900

    rayが飛ばせていることを確認

---
+++ Assets/Script/CameraRay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
+			Debug.Log (hit.collider.gameObject.name);
+		}
+	}
+}
