2544d2351da47875f985c741403c548c00ee4d4a
DESKTOP-UCCMCGD\takab <takabsk55@yahoo.co.jp>
2018-05-04 23:37:27 +0900

    各キャラでcellarrayを取得できていることを確認

--- Assets/Script/Caractor.cs
+++ Assets/Script/Caractor.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Caractor : MonoBehaviour {
 	public GameObject myChara;
+	private Cells cell;
+	private GameObject master;
 	// Use this for initialization
 	void Start () {
-		
+			
 	}
 	
 	// Update is called once per frame
@@ -19,9 +21,10 @@ public class Caractor : MonoBehaviour {
 		return this.transform.position;
 	}
 	void Turn(){
-
+		master = GameObject.Find ("Master");
+		cell = master.GetComponent<Cells>();	
 		Debug.Log (myChara.transform.position.x);
-
+		Debug.Log (cell.CellArray[1,1,1]);
 	}
 }
 
--- Assets/Script/Cells.cs
+++ Assets/Script/Cells.cs
@@ -7,38 +7,41 @@ public class Cells : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		CellArray=new GameObject[4, 4, 2];
-		CellArray [0, 0, 0] = GameObject.Find ("skeleton111");
-		CellArray [1, 0, 0] = GameObject.Find ("skeleton112");
-		CellArray [2, 0, 0] = GameObject.Find ("skeleton113");
-		CellArray [3, 0, 0] = GameObject.Find ("skeleton114");
-		CellArray [0, 1, 0] = GameObject.Find ("skeleton121");
-		CellArray [1, 1, 0] = GameObject.Find ("skeleton122");
-		CellArray [2, 1, 0] = GameObject.Find ("skeleton123");
-		CellArray [3, 1, 0] = GameObject.Find ("skeleton124");
-		CellArray [0, 2, 0] = GameObject.Find ("skeleton131");
-		CellArray [1, 2, 0] = GameObject.Find ("skeleton132");
-		CellArray [2, 2, 0] = GameObject.Find ("skeleton133");
-		CellArray [3, 2, 0] = GameObject.Find ("skeleton134");
-		CellArray [0, 3, 0] = GameObject.Find ("skeleton141");
-		CellArray [1, 3, 0] = GameObject.Find ("skeleton142");
-		CellArray [2, 3, 0] = GameObject.Find ("skeleton143");
-		CellArray [3, 3, 0] = GameObject.Find ("skeleton144");
-		CellArray [0, 0, 1] = GameObject.Find ("skeleton211");
-		CellArray [1, 0, 1] = GameObject.Find ("skeleton212");
-		CellArray [2, 0, 1] = GameObject.Find ("skeleton213");
-		CellArray [3, 0, 1] = GameObject.Find ("skeleton214");
-		CellArray [0, 1, 1] = GameObject.Find ("skeleton221");
-		CellArray [1, 1, 1] = GameObject.Find ("skeleton222");
-		CellArray [2, 1, 1] = GameObject.Find ("skeleton223");
-		CellArray [3, 1, 1] = GameObject.Find ("skeleton224");
-		CellArray [0, 2, 1] = GameObject.Find ("skeleton231");
-		CellArray [1, 2, 1] = GameObject.Find ("skeleton232");
-		CellArray [2, 2, 1] = GameObject.Find ("skeleton233");
-		CellArray [3, 2, 1] = GameObject.Find ("skeleton234");
-		CellArray [0, 3, 1] = GameObject.Find ("skeleton241");
-		CellArray [1, 3, 1] = GameObject.Find ("skeleton242");
-		CellArray [2, 3, 1] = GameObject.Find ("skeleton243");
-		CellArray [3, 3, 1] = GameObject.Find ("skeleton244");
+		CellArray [0, 0, 0] = GameObject.Find ("skelton111");
+		CellArray [1, 0, 0] = GameObject.Find ("skelton112");
+		CellArray [2, 0, 0] = GameObject.Find ("skelton113");
+		CellArray [3, 0, 0] = GameObject.Find ("skelton114");
+		CellArray [0, 1, 0] = GameObject.Find ("skelton121");
+		CellArray [1, 1, 0] = GameObject.Find ("skelton122");
+		CellArray [2, 1, 0] = GameObject.Find ("skelton123");
+		CellArray [3, 1, 0] = GameObject.Find ("skelton124");
+		CellArray [0, 2, 0] = GameObject.Find ("skelton131");
+		CellArray [1, 2, 0] = GameObject.Find ("skelton132");
+		CellArray [2, 2, 0] = GameObject.Find ("skelton133");
+		CellArray [3, 2, 0] = GameObject.Find ("skelton134");
+		CellArray [0, 3, 0] = GameObject.Find ("skelton141");
+		CellArray [1, 3, 0] = GameObject.Find ("skelton142");
+		CellArray [2, 3, 0] = GameObject.Find ("skelton143");
+		CellArray [3, 3, 0] = GameObject.Find ("skelton144");
+		CellArray [0, 0, 1] = GameObject.Find ("skelton211");
+		CellArray [1, 0, 1] = GameObject.Find ("skelton212");
+		CellArray [2, 0, 1] = GameObject.Find ("skelton213");
+		CellArray [3, 0, 1] = GameObject.Find ("skelton214");
+		CellArray [0, 1, 1] = GameObject.Find ("skelton221");
+		CellArray [1, 1, 1] = GameObject.Find ("skelton222");
+		CellArray [2, 1, 1] = GameObject.Find ("skelton223");
+		CellArray [3, 1, 1] = GameObject.Find ("skelton224");
+		CellArray [0, 2, 1] = GameObject.Find ("skelton231");
+		CellArray [1, 2, 1] = GameObject.Find ("skelton232");
+		CellArray [2, 2, 1] = GameObject.Find ("skelton233");
+		CellArray [3, 2, 1] = GameObject.Find ("skelton234");
+		CellArray [0, 3, 1] = GameObject.Find ("skelton241");
+		CellArray [1, 3, 1] = GameObject.Find ("skelton242");
+		CellArray [2, 3, 1] = GameObject.Find ("skelton243");
+		CellArray [3, 3, 1] = GameObject.Find ("skelton244");
+
+		Debug.Log (CellArray[0,0,0].transform.position.x
+		);
 	}
 	
 	// Update is called once per frame
