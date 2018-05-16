1ec7fd87746d31d506f0cb34eb5b401d13eb2dbc
takahiro yamanishi <takabsk55@yahoo.co.jp>
2018-05-16 20:54:11 +0900

    最終発表のスライド追加

--- Assets/Script/Catch.cs
+++ Assets/Script/Catch.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Catch : MonoBehaviour {
 	public TextMesh catchText;
+	public GameObject obj;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,7 +13,8 @@ public class Catch : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		obj = GameObject.Find ("New Text");
+		obj.transform.position =new Vector3 (obj.transform.position.x, obj.transform.position.y, (GameObject.Find ("MainCamera").transform.position.z+GameObject.Find ("Master").transform.position.z)/2);
 	}
 	public void CatchText(){
 		catchText.text="Catch!!!";
